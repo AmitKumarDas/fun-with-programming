b25356972d013c5a996a54a5ab1e1254ea0b3707
AmitKumarDas <amitd2@vmware.com>
2022-09-09 17:22:11 +0530

    feat: add golang based scripting experiments
    Signed-off-by: AmitKumarDas <amitd2@vmware.com>

---
+++ go/shellx-carvel/README.md
@@ -0,0 +1,11 @@
+## Golang scripting to manage releases
+- This codebase showcases use of Golang to script a project's release
+- Note: _Release management is done by Carvel_
+- Motivation:
+  - Similarity to bash but with compile time checks
+  - Granular error handling
+  - Unit tested
+  - Modular code base
+
+## Usage
+- `make`
---
+++ go/shellx-carvel/runtime.go
@@ -0,0 +1,130 @@
+package shellx_carvel
+
+import (
+	"github.com/magefile/mage/sh"
+	"os"
+	"runtime"
+	"strings"
+)
+
+// setEnv sets the given environment key with the given value
+// only if this env key is not set
+func setEnv(envKey, defaultVal string) string {
+	if value := os.Getenv(envKey); value == "" {
+		os.Setenv(envKey, defaultVal)
+	}
+	return os.Getenv(envKey)
+}
+
+func setEnvTrimPrefix(envKey, defaultVal string) string {
+	k := strings.TrimPrefix(envKey, "$")
+	return setEnv(k, defaultVal)
+}
+
+func isErr(err error, more ...error) bool {
+	if err != nil {
+		return true
+	}
+	for _, e := range more {
+		if e != nil {
+			return true
+		}
+	}
+	return false
+}
+
+func isNoErr(err error, more ...error) bool {
+	return !isErr(err, more...)
+}
+
+// Environment variables in a format that can be expanded
+var (
+	EnvGOOS               = "$GOOS"
+	EnvGOARCH             = "$GOARCH"
+	EnvVersion            = "$VERSION"
+	EnvBinPathCarvel      = "$BIN_PATH_CARVEL"
+	EnvBinPathKind        = "$BIN_PATH_KIND"
+	EnvSetupKindCluster   = "$SETUP_KIND_CLUSTER"
+	EnvKindVersion        = "$KIND_VERSION"
+	EnvRegistryName       = "$REGISTRY_NAME"
+	EnvRegistryPort       = "$REGISTRY_PORT"
+	EnvK8sNamespace       = "$K8S_NAMESPACE"
+	EnvK8sServiceAccount  = "$K8S_SERVICE_ACCOUNT"
+	EnvK8sRole            = "$K8S_ROLE"
+	EnvK8sRoleBinding     = "$K8S_ROLE_BINDING"
+	EnvKappCtrlVersion    = "$KAPP_CTRL_VERSION"
+	EnvAppImageName       = "$APP_IMAGE_NAME"
+	EnvAppImageVersion    = "$APP_IMAGE_VERSION"
+	EnvAppBundleName      = "$APP_BUNDLE_NAME"
+	EnvAppBundleVersion   = "$APP_BUNDLE_VERSION"
+	EnvPackageName        = "$PACKAGE_NAME"
+	EnvPackageVersion     = "$PACKAGE_VERSION"
+	EnvPackageRepoName    = "$PACKAGE_REPO_NAME"
+	EnvPackageRepoVersion = "$PACKAGE_REPO_VERSION"
+)
+
+var version = setEnvTrimPrefix(EnvVersion, "v1.0.1")
+
+var binPathCarvel = setEnvTrimPrefix(EnvBinPathCarvel, "tmp")
+var binPathKind = setEnvTrimPrefix(EnvBinPathKind, "tmp")
+
+// carvel binaries / CLIs as functions
+var kbld = sh.RunCmd(binPathCarvel + "/kbld")
+var whichKbld = sh.RunCmd("ls", binPathCarvel+"/kbld")
+var imgpkg = sh.RunCmd(binPathCarvel + "/imgpkg")
+var whichImgpkg = sh.RunCmd("ls", binPathCarvel+"/imgpkg")
+var ytt = sh.RunCmd(binPathCarvel + "/ytt")
+var whichYtt = sh.RunCmd("ls", binPathCarvel+"/ytt")
+
+// kind CLI as function
+var kind = sh.RunCmd(binPathKind + "/kind")
+var whichKind = sh.RunCmd("ls", binPathKind+"/kind")
+
+// unix & generic commands as functions
+var mkdir = sh.RunCmd("mkdir", "-p")
+var curl = sh.RunCmd("curl")
+var ls = sh.RunCmd("ls")
+var chmod = sh.RunCmd("chmod")
+
+func init() {
+	// environment keys & corresponding default values
+	//
+	// Note: This helps in expanding an env variable as $ENV_KEY_NAME
+	// during command execution
+	envs := map[string]string{
+		// OS & Architecture
+		EnvGOOS:   runtime.GOOS,
+		EnvGOARCH: runtime.GOARCH,
+
+		// kind cluster
+		EnvSetupKindCluster: "false",
+		EnvKindVersion:      "v0.15.0",
+
+		// container registry
+		EnvRegistryName: "kind-registry.local",
+		EnvRegistryPort: "5000",
+
+		// k8s rbac
+		EnvK8sNamespace:      "shell-system",
+		EnvK8sServiceAccount: "shell",
+		EnvK8sRole:           "shell-role",
+		EnvK8sRoleBinding:    "shell-role-binding",
+
+		// versions & names
+		EnvKappCtrlVersion:    "v0.40.0",
+		EnvAppImageName:       "tkg-remediator",
+		EnvAppImageVersion:    version,
+		EnvAppBundleName:      "tkg-remediator-app",
+		EnvAppBundleVersion:   version,
+		EnvPackageName:        "tkg-remediator.tanzu.vmware.com",
+		EnvPackageVersion:     version,
+		EnvPackageRepoName:    "tkg-remediator-repo.tanzu.vmware.com",
+		EnvPackageRepoVersion: version,
+	}
+	for k, v := range envs {
+		setEnvTrimPrefix(k, v)
+	}
+
+	// display all the environment variables for debuggability
+	sh.RunV("env")
+}
