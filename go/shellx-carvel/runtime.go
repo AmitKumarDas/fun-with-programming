eac9d29bccc74f621376bbd2535009dc48d3f702
AmitKumarDas <amitd2@vmware.com>
2022-09-12 15:06:50 +0530

    feat: add error handling for command arguments
    This change ensures command arguments that expand to environment
    variables are verified for their presence. This also tries to
    handle env expansions that are invalid.
    
    Note: New logic has been borrowed from os package of go standard library
    
    Signed-off-by: AmitKumarDas <amitd2@vmware.com>

--- go/shellx-carvel/runtime.go
+++ go/shellx-carvel/runtime.go
@@ -1,6 +1,7 @@
 package shellx_carvel
 
 import (
+	"fmt"
 	"github.com/magefile/mage/sh"
 	"os"
 	"runtime"
@@ -35,6 +36,130 @@ func isNoErr(err error, more ...error) bool {
 	return !isErr(err, more...)
 }
 
+// isAlphaNum reports whether the byte is an ASCII letter, number, or underscore
+//
+// Note: Borrowed from os package
+func isAlphaNum(c uint8) bool {
+	return c == '_' || '0' <= c && c <= '9' || 'a' <= c && c <= 'z' || 'A' <= c && c <= 'Z'
+}
+
+// isShellSpecialVar reports whether the character identifies a special
+// shell variable such as $*.
+//
+// Note: Borrowed from os package
+func isShellSpecialVar(c uint8) bool {
+	switch c {
+	case '*', '#', '$', '@', '!', '?', '-', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9':
+		return true
+	}
+	return false
+}
+
+// getShellName returns the name that begins the string and the number of bytes
+// consumed to extract it. If the name is enclosed in {}, it's part of a ${}
+// expansion and two more bytes are needed than the length of the name.
+//
+// Note: Borrowed from os package
+func getShellName(s string) (string, int) {
+	switch {
+	case s[0] == '{':
+		if len(s) > 2 && isShellSpecialVar(s[1]) && s[2] == '}' {
+			return s[1:2], 3
+		}
+		// Scan to closing brace
+		for i := 1; i < len(s); i++ {
+			if s[i] == '}' {
+				if i == 1 {
+					return "", 2 // Bad syntax; eat "${}"
+				}
+				return s[1:i], i + 1
+			}
+		}
+		return "", 1 // Bad syntax; eat "${"
+	case isShellSpecialVar(s[0]):
+		return s[0:1], 1
+	}
+	// Scan alphanumerics.
+	var i int
+	for i = 0; i < len(s) && isAlphaNum(s[i]); i++ {
+	}
+	return s[:i], i
+}
+
+type invalidArgError struct{ Msg string }
+
+func (e *invalidArgError) Error() string { return e.Msg }
+
+// expandEnvStrict replaces ${var} or $var in the string. It returns
+// error for invalid use of $ or env expansion returns empty
+//
+// Note: Borrowed from os.ExpandEnv
+func expandEnvStrict(s string) (string, error) {
+	var buf []byte
+	// ${} is all ASCII, so bytes are fine for this operation.
+	i := 0
+	for j := 0; j < len(s); j++ {
+		if s[j] == '$' && j+1 < len(s) {
+			if buf == nil {
+				buf = make([]byte, 0, 2*len(s))
+			}
+			buf = append(buf, s[i:j]...)
+			name, w := getShellName(s[j+1:])
+			if name == "" && w > 0 {
+				// Encountered invalid syntax
+				return "", &invalidArgError{fmt.Sprintf("invalid %s", s)}
+			} else if name == "" {
+				// Valid syntax, but $ was not followed by a
+				// name. Leave the dollar character untouched.
+				buf = append(buf, s[j])
+			} else {
+				val, found := os.LookupEnv(name)
+				if !found {
+					return "", &invalidArgError{fmt.Sprintf("%s lookup failed", name)}
+				}
+				buf = append(buf, val...)
+			}
+			j += w
+			i = j + 1
+		}
+	}
+	if buf == nil {
+		return s, nil
+	}
+	return string(buf) + s[i:], nil
+}
+
+// verifyArgs returns error if the arguments makes use of unset
+// environment variables
+func verifyArgs(args ...string) error {
+	if len(args) == 0 {
+		return nil
+	}
+	var invalidArgs = make([]string, 0, len(args))
+	for _, arg := range args {
+		if _, err := expandEnvStrict(arg); err != nil {
+			invalidArgs = append(invalidArgs, err.Error())
+		}
+	}
+	if len(invalidArgs) == 0 {
+		return nil
+	}
+	return &invalidArgError{fmt.Sprintf("verify args ['%s']: %s", strings.Join(args, "', '"), strings.Join(invalidArgs, ", "))}
+}
+
+// runCmdStrict is a wrapper over sh.Run. The returned function
+// returns error if either verification of arguments fails or
+// command execution fails
+func runCmdStrict(cmd string, args ...string) func(args ...string) error {
+	return func(args2 ...string) error {
+		allArgs := append(args, args2...)
+		if err := verifyArgs(allArgs...); err != nil {
+			return err
+		}
+		return sh.Run(cmd, allArgs...)
+	}
+}
+
 // Environment variables in a format that can be expanded
 var (
 	EnvGOOS               = "$GOOS"
@@ -67,25 +192,25 @@ var binPathCarvel = maybeSetEnvTrimKey(EnvBinPathCarvel, "tmp")
 var binPathKind = maybeSetEnvTrimKey(EnvBinPathKind, "tmp")
 
 // Carvel binaries / CLIs as functions
-var kbld = sh.RunCmd(binPathCarvel + "/kbld")
-var whichKbld = sh.RunCmd("ls", binPathCarvel+"/kbld")
-var imgpkg = sh.RunCmd(binPathCarvel + "/imgpkg")
-var whichImgpkg = sh.RunCmd("ls", binPathCarvel+"/imgpkg")
-var ytt = sh.RunCmd(binPathCarvel + "/ytt")
-var whichYtt = sh.RunCmd("ls", binPathCarvel+"/ytt")
+var kbld = runCmdStrict(binPathCarvel + "/kbld")
+var whichKbld = runCmdStrict("ls", binPathCarvel+"/kbld")
+var imgpkg = runCmdStrict(binPathCarvel + "/imgpkg")
+var whichImgpkg = runCmdStrict("ls", binPathCarvel+"/imgpkg")
+var ytt = runCmdStrict(binPathCarvel + "/ytt")
+var whichYtt = runCmdStrict("ls", binPathCarvel+"/ytt")
 
 // KIND CLI as function
-var kind = sh.RunCmd(binPathKind + "/kind")
-var whichKind = sh.RunCmd("ls", binPathKind+"/kind")
+var kind = runCmdStrict(binPathKind + "/kind")
+var whichKind = runCmdStrict("ls", binPathKind+"/kind")
 
 // Docker CLI as function
-var docker = sh.RunCmd("docker")
+var docker = runCmdStrict("docker")
 
 // Unix & generic commands as functions
-var mkdir = sh.RunCmd("mkdir", "-p")
-var curl = sh.RunCmd("curl")
-var ls = sh.RunCmd("ls")
-var chmod = sh.RunCmd("chmod")
+var mkdir = runCmdStrict("mkdir", "-p")
+var curl = runCmdStrict("curl")
+var ls = runCmdStrict("ls")
+var chmod = runCmdStrict("chmod")
 
 func init() {
 	// environment keys & corresponding default values
