729acf5e2791407eec4ee557e98e02eea308dede
AmitKumarDas <amitd2@vmware.com>
2022-09-12 15:09:52 +0530

    feat: add error handling for invalid arguments
    Signed-off-by: AmitKumarDas <amitd2@vmware.com>

--- go/shellx-carvel/docker.go
+++ go/shellx-carvel/docker.go
@@ -1,7 +1,13 @@
 package shellx_carvel
 
+import "errors"
+
 func setupRegistryAsLocalDockerContainer() error {
 	if err := docker("inspect", "-f", "{{.State.Running}}", "${REGISTRY_NAME}"); err != nil {
+		var envErr *invalidArgError
+		if errors.As(err, &envErr) {
+			return err
+		}
 		return docker("run", "-d", "--restart", "always", "-p", "127.0.0.1:${REGISTRY_PORT}:5000", "--name", "${REGISTRY_NAME}", "registry:2")
 	}
 	return nil
