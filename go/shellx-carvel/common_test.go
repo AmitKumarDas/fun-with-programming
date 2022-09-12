6d20e1aea48d499ba1ebf81e22a72438f769596d
AmitKumarDas <amitd2@vmware.com>
2022-09-12 15:05:45 +0530

    feat: add common test functions for assertion
    Signed-off-by: AmitKumarDas <amitd2@vmware.com>

--- go/shellx-carvel/common_test.go
+++ go/shellx-carvel/common_test.go
@@ -2,6 +2,20 @@ package shellx_carvel
 
 import "testing"
 
+func requireCount(t *testing.T, expected int, actual int) {
+	if expected == actual {
+		return
+	}
+	t.Fatalf("expected %d got %d", expected, actual)
+}
+
+func requireErr(t *testing.T, err error) {
+	if err != nil {
+		return
+	}
+	t.Fatal("expected error got none")
+}
+
 func requireNoErr(t *testing.T, err error) {
 	if err == nil {
 		return
@@ -9,8 +23,8 @@ func requireNoErr(t *testing.T, err error) {
 	t.Fatal(err)
 }
 
-func requireTrue(t *testing.T, given bool) {
-	if given {
+func requireTrue(t *testing.T, actual bool) {
+	if actual {
 		return
 	}
 	t.Fatal("expected true got false")
