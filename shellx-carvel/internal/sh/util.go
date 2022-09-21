72e8cb2b060bfffa3ee27cee264bd23a89b2c0eb
AmitKumarDas <amitd2@vmware.com>
2022-09-21 14:48:58 +0530

    feat: add IsEq feature to internal sh package
    Signed-off-by: AmitKumarDas <amitd2@vmware.com>

--- shellx-carvel/internal/sh/util.go
+++ shellx-carvel/internal/sh/util.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"os"
 	"path"
+	"strings"
 )
 
 func File(name, data string, perm os.FileMode) error {
@@ -23,4 +24,16 @@ func JoinPaths(paths ...string) (string, error) {
 		return "", err
 	}
 	return path.Join(expanded...), nil
+}
+
+// IsEq verifies if the provided strings are equal. The string(s)
+// can also be an env variable or consist of an env variable
+// as a substring. ENV variable if any will have its value
+// matched.
+//
+// Note: It will mostly return false for invalid env syntax or
+// strings with special shell characters s.a $*, $#, etc.
+// Refer os.ExpandEnv & os.Expand for more details
+func IsEq(a, b string) bool {
+	return strings.ToLower(os.ExpandEnv(a)) == strings.ToLower(os.ExpandEnv(b))
 }
--- shellx-carvel/internal/sh/util_test.go
+++ shellx-carvel/internal/sh/util_test.go
@@ -10,3 +10,21 @@ func TestFile(t *testing.T) {
 	requireErr(t, File("tmp/will-not-create.txt", "I $WILL_NOT_EXIST due to this unset env\n", 0644))
 	requireNoErr(t, File("tmp/will-create.txt", "I WILL EXIST\n", 0644))
 }
+
+func TestIsEq(t *testing.T) {
+	// mock env k:v pairs
+	EnvTestIsEq := "${TEST_IS_EQ}"
+	_ = MaybeSetEnv(EnvTestIsEq, "testing")
+	defer func() {
+		UnsetEnv(EnvTestIsEq)
+	}()
+	t.Run("match empty value given an unset env", func(t *testing.T) {
+		requireTrue(t, IsEq("${DOES_NOT_EXIST}", ""))
+	})
+	t.Run("match actual value given a set env", func(t *testing.T) {
+		requireTrue(t, IsEq(EnvTestIsEq, "testing"))
+	})
+	t.Run("match substring given a string containing an unset env", func(t *testing.T) {
+		requireTrue(t, IsEq("Who am ${I}?", "Who am ?"))
+	})
+}
