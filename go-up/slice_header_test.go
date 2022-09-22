9d050176ac3fb21b4ca0c2e8f161be43079789c1
AmitKumarDas <amitd2@vmware.com>
2022-09-22 14:51:43 +0530

    tests: update slice header experiment as a sub test
    Signed-off-by: AmitKumarDas <amitd2@vmware.com>

--- go-up/slice_header_test.go
+++ go-up/slice_header_test.go
@@ -6,10 +6,13 @@ import (
 	"unsafe"
 )
 
-func TestSliceHeaderOfNilArray(t *testing.T) {
-	// https://go.dev/play/p/hsIOG1wYBhc
-	var s []string = nil
-	h := (*reflect.SliceHeader)(unsafe.Pointer(&s))
-
-	requireTrue(t, *h == reflect.SliceHeader{Data: 0, Len: 0, Cap: 0})
+func TestSliceHeader(t *testing.T) {
+	// Slice with pointer 0 and length 0 is exactly the nil slice
+	t.Run("SliceHeader{Data: 0, Len: 0, Cap: 0}) is nil slice", func(t *testing.T) {
+		// https://go.dev/play/p/hsIOG1wYBhc
+		var s []string = nil
+		h := (*reflect.SliceHeader)(unsafe.Pointer(&s))
+		requireTrue(t, h != nil)
+		requireTrue(t, *h == reflect.SliceHeader{Data: 0, Len: 0, Cap: 0}) // TIL
+	})
 }
