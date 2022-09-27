aec2869f1fe104499b9f88c86694b21e9b40f230
AmitKumarDas <amitd2@vmware.com>
2022-09-27 13:39:41 +0530

    feat: add subtests to build, publish, & test carvel package
    Running `make test-carvel-release-info` gives similar output as follows:
    
    --- PASS: TestReleaseOfCarvelPackage (39.51s)
        --- PASS: TestReleaseOfCarvelPackage/installing_carvel_CLIs (0.10s)
        --- PASS: TestReleaseOfCarvelPackage/installing_KIND_cli (0.05s)
        --- PASS: TestReleaseOfCarvelPackage/setting_up_local_docker_container_as_an_OCI_registry (0.17s)
        --- PASS: TestReleaseOfCarvelPackage/publishing_the_app_bundle (9.91s)
        --- PASS: TestReleaseOfCarvelPackage/publishing_the_package_release (5.82s)
        --- PASS: TestReleaseOfCarvelPackage/setting_up_KIND_cluster (0.74s)
        --- PASS: TestReleaseOfCarvelPackage/verifying_app_deployment (22.72s)
    PASS
    ok  	carvel.shellx.dev	40.093s
    
    Signed-off-by: AmitKumarDas <amitd2@vmware.com>

--- shellx-carvel/main_test.go
+++ shellx-carvel/main_test.go
@@ -3,11 +3,35 @@ package shellx_carvel
 import "testing"
 
 func TestReleaseOfCarvelPackage(t *testing.T) {
-	tryInstallCarvelCLIs(t)
-	tryInstallKindCLI(t)
-	trySetupRegistryAsLocalDockerContainer(t)
-	tryCutAppBundle(t)
-	tryCutPackageRelease(t)
-	trySetupKindCluster(t)
-	tryVerifyApplication(t)
+
+	// Note:
+	// -------------------------------------------------------
+	// Naming convention followed for each subtest:
+	// Running this testcase will verify {name of subtest}
+	//
+	// Where {name of subtest} is mentioned as shown below:
+	// t.Run("name of subtest", func(t *testing.T){...})
+	// -------------------------------------------------------
+
+	t.Run("installing carvel CLIs", func(t *testing.T) {
+		tryInstallCarvelCLIs(t)
+	})
+	t.Run("installing KIND cli", func(t *testing.T) {
+		tryInstallKindCLI(t)
+	})
+	t.Run("setting up local docker container as an OCI registry", func(t *testing.T) {
+		trySetupRegistryAsLocalDockerContainer(t)
+	})
+	t.Run("publishing the app bundle", func(t *testing.T) {
+		tryCutAppBundle(t)
+	})
+	t.Run("publishing the package release", func(t *testing.T) {
+		tryCutPackageRelease(t)
+	})
+	t.Run("setting up KIND cluster", func(t *testing.T) {
+		trySetupKindCluster(t)
+	})
+	t.Run("verifying app deployment", func(t *testing.T) {
+		tryVerifyApplication(t)
+	})
 }
