fb63283e539632a8c3cd70984f686e0d22f02e53
AmitKumarDas <amitd2@vmware.com>
2022-10-03 16:10:28 +0530

    fix: remove registry before creating the same ensures a clean setup
    This change ensures test runs are idiomatic
    
    Signed-off-by: AmitKumarDas <amitd2@vmware.com>

--- shellx-carvel/docker.go
+++ shellx-carvel/docker.go
@@ -2,21 +2,13 @@ package shellx_carvel
 
 import (
 	shx "carvel.shellx.dev/internal/sh"
-	"errors"
 )
 
 func setupRegistryAsLocalDockerContainer() error {
 	if shx.IsNotEq(EnvSetupLocalRegistry, "true") {
 		return nil
 	}
-	if err := docker("inspect", "-f", "{{.State.Running}}", EnvRegistryName); err != nil {
-		var envErr *shx.InvalidEnvError // Note: Must be a pointer
-		if errors.As(err, &envErr) {
-			return err
-		}
-		// Note: This error is mostly due to docker in NOT RUNNING state
-		// Start a local registry as docker service
-		return docker("run", "-d", "--restart", "always", "-p", format("127.0.0.1:%s:5000", EnvRegistryPort), "--name", EnvRegistryName, "registry:2")
-	}
-	return nil
+	_ = docker("stop", EnvRegistryName)
+	_ = docker("rm", EnvRegistryName)
+	return docker("run", "-d", "--restart", "always", "-p", format("127.0.0.1:%s:5000", EnvRegistryPort), "--name", EnvRegistryName, "registry:2")
 }
